6dedc14904c41882a25af7050c2f2c677ad2ce75
Noro <stepanyan.norayr93@gmail.com>
2020-01-13 18:05:41 +0400

    Updates

---
+++ precache-manifest.fed8cbc9aaabf93c6e1f93f1781fdf95.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05b22bdb5e33f5ca53472c36b20b082a",
+    "url": "/My-work/index.html"
+  },
+  {
+    "revision": "44016fffa86f9f2b4c46",
+    "url": "/My-work/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "13dc6b26c68749f9b25d",
+    "url": "/My-work/static/css/3.b126a189.chunk.css"
+  },
+  {
+    "revision": "95a484a90a89275413f1",
+    "url": "/My-work/static/css/4.d4dfd1a5.chunk.css"
+  },
+  {
+    "revision": "78564c96e5c637731402",
+    "url": "/My-work/static/css/main.a36b5ce0.chunk.css"
+  },
+  {
+    "revision": "44016fffa86f9f2b4c46",
+    "url": "/My-work/static/js/2.d2034d10.chunk.js"
+  },
+  {
+    "revision": "13dc6b26c68749f9b25d",
+    "url": "/My-work/static/js/3.dec795cd.chunk.js"
+  },
+  {
+    "revision": "95a484a90a89275413f1",
+    "url": "/My-work/static/js/4.cf68d3dd.chunk.js"
+  },
+  {
+    "revision": "78564c96e5c637731402",
+    "url": "/My-work/static/js/main.37b16fb5.chunk.js"
+  },
+  {
+    "revision": "afaa24161afb2a623dfe",
+    "url": "/My-work/static/js/runtime-main.c9f6545a.js"
+  },
+  {
+    "revision": "ea3271aeb738003d8c0ffd424d181e6c",
+    "url": "/My-work/static/media/preLoader.ea3271ae.gif"
+  }
+]);
